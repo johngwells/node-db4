7532b089eebd1dbc8784c8e3c815e00fcb2a116e
John Wells <molekewl2017@gmail.com>
2020-03-21 13:27:45 -0700

    recipe model functions

---
+++ recipes/recipes-model.js
@@ -0,0 +1,26 @@
+const db = require('../data/db-config');
+
+module.exports = {
+  getRecipes,
+  getShoppingList,
+  getInstructions
+}
+
+function getRecipes() {
+  return db('recipes');
+}
+
+function getList(recipe_id) {
+  return db('recipe_ingredients')
+    .join('ingredients', 'recipe_ingredients.ingredient_id', '=', 'ingredients.id')
+    .select('name', 'quantity', 'unit')
+    .where({recipe_id})
+    .orderBy('name')
+}
+
+function getInstructions(recipe_id) {
+  return db('recipe_steps')
+    .select('recipe_steps')
+    .where({recipe_id})
+    .orderBy('step')
+}
